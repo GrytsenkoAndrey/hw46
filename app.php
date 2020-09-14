84d82fb02d7ba6f0107f092d855c310ff97a0b89
Grytsenko Andrey <andreyapg@gmail.com>
2020-09-14 21:14:14 +0300

    say-hello-it application (interactive)

--- app.php
+++ app.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
-$app = new \Symfony\Component\Console\Application('Format string');
+$app = new \Symfony\Component\Console\Application('Interactive application');
 
-#$app->add(new \App\FormatString());
-#$app->run();
+$app->add(new \App\SayHelloIt());
+$app->run();
