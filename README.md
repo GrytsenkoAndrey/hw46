6cae0572301e2afd49732cdfe68803e04ca59d4c
Grytsenko Andrey <andreyapg@gmail.com>
2020-09-14 18:22:43 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Home work 4-6
+Console application for module 4-6
+## Dependencies
+Using Symfony console
+
---
+++ app.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+$app = new \Symfony\Component\Console\Application('Format string');
+
+#$app->add(new \App\FormatString());
+#$app->run();
